5ad6504f92f2e41bc5284d8d1422503e3df989f6
Susancutie <susancutie@qq.com>
2022-06-21 19:29:02 +0800

    feature：POST获取团队列表

---
+++ src/apis/team.js
@@ -0,0 +1,28 @@
+import service from "utils/request";
+
+/**
+ * @description: 获取team列表
+ * @param {*} data
+ * @return {*}
+ */
+export const addDeposit = (data) => {
+  return service({
+    url: "/admin/good/addDeposit",
+    method: "post",
+    data,
+  });
+};
+
+
+/**
+ * @description: 获取team列表
+ * @param {*} data
+ * @return {*}
+ */
+export const getTeams = (data) => {
+  return service({
+    url: "http://10.131.167.11:8082/team/getTeams",
+    method: "post",
+    data,
+  });
+};
