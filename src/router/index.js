31b31f45755b929a47f2d5f924ece8c73713dee9
cytmo <cytmo@qq.com>
2022-06-23 10:39:40 +0800

    fix. path after pack

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { getToken } from "utils/auth";
 
 const routes = [
@@ -91,7 +91,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(''),
+  history: createWebHashHistory(''),
   routes,
 });
 
